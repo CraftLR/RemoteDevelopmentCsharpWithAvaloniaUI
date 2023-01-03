c70c0d016bc344913fd6c257226bd1be76bbeb73
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2023-01-03 16:33:51 +0100

    Add AvaloniaUI MVVM sample

---
+++ src/CraftLR.MVVM/Program.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
+using System;
+
+namespace CraftLR.MVVM;
+
+class Program
+{
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace()
+            .UseReactiveUI();
+}
---
+++ src/CraftLR.MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿
+namespace CraftLR.MVVM.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
